1b5f3392eef64002899bb48747bda841cdc01ad1
Eel1s0 <s2300903@edu.bc.fi>
2025-04-23 09:28:17 +0300

    Harjoitus 2

---
+++ ApiHarjoitus2/Program.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Lis‰‰ controller-tuki
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// K‰ytet‰‰n routeja
+app.MapControllers();
+
+app.Run();
